e16639b479b11f9642ccfa2495dca7ca04c27fdb
Angel Rodriguez <angel-antonio@live.com>
2018-06-18 00:53:35 -0400

    added mongodb connection with mongoose, using keys from file and tested error catching/promises

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://dasmedium:3xtrat3rr3str3@ds163510.mlab.com:63510/dmsocial"
+    "mongodb://dmadminuser:3xtrat3rr3str3@ds163510.mlab.com:63510/dmsocial"
 };
 // Might use different key files for Dev or production environments depending on different databases used.
