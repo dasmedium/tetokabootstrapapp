940dc949f4401c7840033d1fc2180d118be08afb
Angel Rodriguez <angel-antonio@live.com>
2018-06-18 23:33:41 -0400

    added more validation conditons

--- validation/register.js
+++ validation/register.js
@@ -16,6 +16,25 @@ module.exports = function validateRegisterInput(data) {
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+  if (Validator.isEmail(data.name)) {
+    errors.email = "Email is invalid";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  if ((Validator.isLength(data.password), { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "confirm Password field is required";
+  }
+  if (Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+
   return {
     errors,
     // errors is an object not a string, this must have to do with including different error keys to make for better error reporting, hence why the need for turning the isEmpty(str) into a isEmpty({object}). isValid returns true if isEmpty returns true (or any errors passed in our validation)
