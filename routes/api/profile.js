c063ff172dc7edff877ad03191bd99f05e1f55ce
Angel Rodriguez <angel-antonio@live.com>
2018-06-22 09:27:27 -0400

    added /handle/:handle and /user/:user_id backend routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -37,6 +37,40 @@ router.get(
   }
 );
 
+// @route   GET api/profile/handle/:handle -- its a backend api route, in browser its diff
+// @desc    Get profile by handle
+// @access  Public
+
+router.get("/handle/:handle", (req, res) => {
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/profile/user/:user_id -- its a backend api route, in browser its diff
+// @desc    Get profile by handle
+// @access  Public
+
+router.get("/user/:user_id", (req, res) => {
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 // @route   POST api/profile
 // @desc    Create or edit user profile
 // @access  Private
