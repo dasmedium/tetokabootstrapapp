ad1f153a57a0720a136a77e01377db74846663d8
Angel Rodriguez <angel-antonio@live.com>
2018-06-23 13:04:08 -0400

    added post POST route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Post = require("../../models/post");
 
 // @route   GET api/posts/test
 // @desc    Test posts route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      // we can pull these values from app state - redux
+      avatar: req.body.name,
+      user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
+  }
+);
 module.exports = router;
