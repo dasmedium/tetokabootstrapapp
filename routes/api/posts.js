6b8b00842a768f48292bbfd96dce34857a35c72f
Angel Rodriguez <angel-antonio@live.com>
2018-06-23 15:49:18 -0400

    added unlike POST route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -112,4 +112,39 @@ router.post(
     });
   }
 );
+
+// @route   POST api/posts/unlike/:id
+// @desc    Unlike Post
+// @access  Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check if user liked this post
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "You have not yet liked this post" });
+          }
+          // Get remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          // splice out of array
+          post.likes.splice(removeIndex, 1);
+
+          // Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "Post no found" }));
+    });
+  }
+);
 module.exports = router;
