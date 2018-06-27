bbbb1eb3c9e453873f0ea7a49138963e27b2c6f5
Angel Rodriguez <angel-antonio@live.com>
2018-06-27 00:48:13 -0400

    added axios to test register form and added user to database

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Register extends Component {
   constructor() {
@@ -27,7 +28,12 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    console.log(newUser);
+
+    //Testing api call with axios before redux
+    axios
+      .post("/api/users/register", newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
   }
 
   render() {
