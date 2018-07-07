266e9606d06c399a91929503a897abf814efe894
Angel Rodriguez <angel-antonio@live.com>
2018-07-06 23:46:02 -0400

    started our ProfileGithub component and registered application as Oath app on github

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 class ProfileGithub extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clientId: "2ee654ec73aa0e0177d7",
+      clientSecret: "4e16c0108c927e42276495d8a2c8cdc2d763bff4",
+      count: 5,
+      sort: "created: asc",
+      repos: []
+    };
+  }
+
   render() {
     return (
       <div>
