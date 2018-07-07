dfca49274f9b3fbcfd10b74b5651d92daa34fd80
Angel Rodriguez <angel-antonio@live.com>
2018-07-06 22:52:28 -0400

    added header bootstrap theme elements and formatted to react, conditionally display social links based on profile.social object

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -35,7 +35,7 @@ class Profile extends Component {
             </div>
             <div className="col-md-6" />
           </div>
-          <ProfileHeader />
+          <ProfileHeader profile={profile} />
           <ProfileAbout />
           <ProfileCreds />
           <ProfileGithub />
