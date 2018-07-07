a196b16c0de17ec4f89674df22d97af1930f5527
Angel Rodriguez <angel-antonio@live.com>
2018-07-07 01:16:04 -0400

    created PostForm component and brought in all redux dependencies to use as props, handled onChange and onSubmit and called addPost action

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -7,21 +7,20 @@ import Spinner from "../common/Spinner";
 class Posts extends Component {
   render() {
     return (
-        <div className="feed">
+      <div className="feed">
         <div className="container">
-        <div className="row">
-        <div className="col-md-12">
-        <PostForm />
+          <div className="row">
+            <div className="col-md-12">
+              <PostForm />
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-        </div>
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-    post: state.post;
-
-})
+const mapStateToProps = state => ({
+  post: state.post
+});
 export default connect(mapStateToProps)(Posts);
